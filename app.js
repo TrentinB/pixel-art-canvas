556ab113f92639bc0442ec145dfebcd34205f1c7
TrentinB <trentin.barnhart@gmail.com>
2024-02-17 16:48:04 -0600

    finished basics tutorial
    I finished the basics tutorial for a quick refresher

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const colorButton = document.querySelector('.btn');
+
+colorButton.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if (className == "light-theme"){
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log(className);
+});

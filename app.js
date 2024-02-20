1b5c0b6d1193db9fa8fe41ceeb016b5c32ca9913
TrentinB <trentin.barnhart@gmail.com>
2024-02-20 15:20:35 -0600

    switched to canvas
    html5 has the canvas item which solves this problem already and will be simpler for me to use. I have set up the basic canvas item and will continue development with it instead

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
 'use strict';
 
-const themeButton = document.querySelector('.btn');
-const pixel = document.querySelector('.pixel')
-
-
-themeButton.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if (className == "light-theme"){
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-});
-
-pixel.addEventListener('click', function() {
-    this.style.background = "var(--selectedColor)";
-});
-
-
+var canvas= document.getElementById("canvas");
 
---
+++ canvas.js
@@ -0,0 +1,17 @@
+class canvas{
+    constructor(height, width){
+        //initialize canvas properties
+    }
+
+    draw(x, y){
+        //draw method
+    }
+
+    erase(x, y){
+        //erase method
+    }
+
+    save(){
+        //save image of canvas
+    }
+}

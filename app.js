ec5751ea24b7638dd6018e095f3f7dd68bcde6f9
TrentinB <trentin.barnhart@gmail.com>
2024-02-22 17:08:38 -0600

    draw method called when canvas is clicked
    class is initialized on webpage startup and when the canvas is clicked, the draw function within the class is called. Currently it only outputs to the console. The file 'app.js' is now redundant. Once the code within has been relocated I will delete it. All relevant js code is in 'canvas.js'

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+// this file is now redundant and unused. Delete once code has been relocated
+
 'use strict';
 
 var canvas= document.getElementById("canvas");
 
+canvas.addEventListener("mousemove", MouseEvent => { 
+    if (this.active) { 
+        var rect = canvas.getBoundingClientRect(); //gets the canvas rectangle dimensions and locations
+        var x = MouseEvent.clientX - rect.left; //gets mouse x coordinate relative to canvas
+        var y = MouseEvent.clientY - rect.top; //gets mouse y coordinate relative to canvas
+
+        //rounds coordinates down to nearest pixel coordinate 
+        x = Math.floor(this.width * x / canvas.clientWidth);
+        y = Math.floor(this.height * y / canvas.clientHeight); 
+
+        this.draw(x, y);
+    } 
+});
--- canvas.js
+++ canvas.js
@@ -1,10 +1,13 @@
 class canvas{
-    constructor(height, width){
-        //initialize canvas properties
+
+    constructor(object, height, width){
+        this.object = object; //object is the html canvas
+        this.height = height;
+        this.width = width;
     }
 
     draw(x, y){
-        //draw method
+        console.log("I am drawing");
     }
 
     erase(x, y){
@@ -14,4 +17,12 @@ class canvas{
     save(){
         //save image of canvas
     }
+
 }
+
+var myCanvas = new canvas(document.getElementById("canvas"), 16, 16); //myCanvas initialization
+
+//myCanvas event listener initialization
+myCanvas.object.addEventListener("click", function(){
+    myCanvas.draw(0,0);
+});

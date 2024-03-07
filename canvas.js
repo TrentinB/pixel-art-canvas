acef7ad5d6575781c43b649e9cd1f3bab909d042
TrentinB <trentin.barnhart@gmail.com>
2024-03-07 13:04:59 -0600

    draw but not really
    clicking the canvas now calls the proper canvas drawing method. Proper X and Y coordinates still need to be passed and math will need done to make the pixels work as intended before it's really drawing anything.

--- canvas.js
+++ canvas.js
@@ -4,10 +4,17 @@ class canvas{
         this.object = object; //object is the html canvas
         this.height = height;
         this.width = width;
+
+        //context data may need to be moved to new methods
+        this.ctx = document.getElementById("canvas").getContext("2d");
+        this.ctx.fillStyle = "red";
     }
 
     draw(x, y){
         console.log("I am drawing");
+
+        //fillRect(x, y, width, height)
+        this.ctx.fillRect(0, 0, 150, 75);
     }
 
     erase(x, y){
